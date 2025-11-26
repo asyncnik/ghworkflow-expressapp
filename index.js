009d0f7e81ef0e48b9ae632437fb19827790e4b7
asyncnik <pareeknikhil397@gmail.com>
2025-11-26 22:29:05 +0530

    first for gh demo

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const cors = require("cors")
+const app = express() 
+
+app.use(cors())
+
+app.post("/login", (req, res) => {
+	res.json({"success":1})
+})
+
+app.listen(3000, ()=>console.log("server is running on 3000"))
