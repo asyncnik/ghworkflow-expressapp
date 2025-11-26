cf15b2ec4fc11ba3d81efd81773a09e299596f4d
asyncnik <pareeknikhil397@gmail.com>
2025-11-26 22:34:36 +0530

    second commit - chekcing gh workflow

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ app.post("/login", (req, res) => {
 	res.json({"success":1})
 })
 
+
+app.post("/sinup", (req, res) => {
+	res.json({"success":1})
+})
+
 app.listen(3000, ()=>console.log("server is running on 3000"))
