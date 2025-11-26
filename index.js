b0b7cf8399994c10ad480c0b6731dc6f4399145b
asyncnik <pareeknikhil397@gmail.com>
2025-11-26 22:45:29 +0530

    added test casesso that ci can run easily

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express")
 const cors = require("cors")
-const app = express() 
+const app = express()
 
 app.use(cors())
 
 app.post("/login", (req, res) => {
-	res.json({"success":1})
+	res.json({ success: 1 })
 })
-
 
 app.post("/sinup", (req, res) => {
-	res.json({"success":1})
+	res.json({ success: 1 })
 })
 
-app.listen(3000, ()=>console.log("server is running on 3000"))
+module.exports = app
+
+if (require.main === module) {
+	app.listen(3000, () => console.log("server is running on 3000"))
+}
---
+++ test/app.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest')
+const app = require('../index')
+
+describe('API endpoints', () => {
+  test('POST /login responds with success:1', async () => {
+    const res = await request(app).post('/login')
+    expect(res.statusCode).toBe(200)
+    expect(res.headers['content-type']).toMatch(/json/)
+    expect(res.body).toEqual({ success: 1 })
+  })
+
+  test('POST /sinup responds with success:1', async () => {
+    const res = await request(app).post('/sinup')
+    expect(res.statusCode).toBe(200)
+    expect(res.headers['content-type']).toMatch(/json/)
+    expect(res.body).toEqual({ success: 1 })
+  })
+})
